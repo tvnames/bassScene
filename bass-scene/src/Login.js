fa080060972869262a9b5bd0e088e6c7626f4069
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-10-27 14:00:07 -0700

    added spotify auth_url and react components

---
+++ bass-scene/src/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3001&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+
+export default function Login() {
+  return <div></div>;
+}
