132813aa34dc2c3075ced6cf62b9c3b260dcc301
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-10-27 14:59:06 -0700

    added code to create/start server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const SpotifyWebApi =  require('spotify-web-api-node')
+
+const app = express();
+
+app.post('/login', function (req, res) => {
+    const code = req.body.code
+    const spotifyWebApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3001',
+        clientId: 'a0406b7aa4db4039bdec1d657470e353',
+        clientSecret: '072f53a6584b4e61a96182335063f807',
+    })
+
+    spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+        })
+    })
+    .catch(() => {
+        res.sendStatus(404)
+    })
+})
